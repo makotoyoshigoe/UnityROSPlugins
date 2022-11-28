9f0a897d31cd2bd826e2f3da3b2e9517c034f748
makotoyoshigoe <myoshigo0127@gmail.com>
2022-11-28 21:06:45 +0900

    ジョイントの回転角度を取得できるようにした

--- Scripts/Runtime/JointStatesPublisher.cs
+++ Scripts/Runtime/JointStatesPublisher.cs
@@ -9,22 +9,25 @@ namespace Sample.UnityROSPlugins
     public class JointStatesPublisher : MonoBehaviour
     {
         public List<GameObject> jointStatePublishObjects;
+        private List<ArticulationBody> jointStatePublishArticulationBodys;
         public string topicName = "joint_states";
         private JointStateMsg jointStateMsg = new JointStateMsg();
         private List<JointStateMsg> jointStateMsgList = new List<JointStateMsg>();
         public GameObject ROSConnectionCommon;
         private Commons commons;
         private ROSConnection ros;
+
         // Start is called before the first frame update
         void Start()
         {
             List<string> names = new List<string>();
+            jointStatePublishArticulationBodys = new List<ArticulationBody>();
             for(int i=0; i<jointStatePublishObjects.Count; ++i){
                 Debug.Log(jointStatePublishObjects[i].name);
                 names.Add(jointStatePublishObjects[i].name);
+                jointStatePublishArticulationBodys.Add(jointStatePublishObjects[i].GetComponent<ArticulationBody>());
             }
             commons = ROSConnectionCommon.GetComponent<Commons>();
-            if(commons == null) Debug.Log("Null");
             commons.ros.RegisterPublisher<JointStateMsg>(topicName, 1);
             jointStateMsg.name = names.ToArray();
         }
@@ -32,10 +35,20 @@ namespace Sample.UnityROSPlugins
         // Update is called once per frame
         void FixedUpdate()
         {
-            commons.SetTime(jointStateMsg.header.stamp);
-            commons.ros.Publish(topicName, jointStateMsg);
+            // commons.SetTime(jointStateMsg.header.stamp);
+            // commons.ros.Publish(topicName, jointStateMsg);
+            GetJointPosition(jointStatePublishArticulationBodys);
         }
 
-        // void Set
+        void GetJointPosition(List<ArticulationBody> articulationBodies){
+            foreach(ArticulationBody articulationBody in articulationBodies){
+                List<float> floats = new List<float>();
+                articulationBody.GetJointPositions(floats);
+                // Debug.Log($"{articulationBody.name}: {floats.Count}");
+                // foreach(float f in floats){
+                Debug.Log($"{articulationBody.name}: {floats[floats.Count-3]}");
+                // }
+            }
+        }
     }
 }
