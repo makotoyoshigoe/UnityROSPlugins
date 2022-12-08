3523534c41e8f221886621a262d7fb915c65716b
makotoyoshigoe <myoshigo0127@gmail.com>
2022-12-08 14:07:19 +0900

    /joint_statesを出せるようにした

--- Scripts/Runtime/JointStatesPublisher.cs
+++ Scripts/Runtime/JointStatesPublisher.cs
@@ -2,81 +2,107 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Robotics.ROSTCPConnector;
+using Unity.Robotics;
 using JointStateMsg = RosMessageTypes.Sensor.JointStateMsg;
 
 namespace Sample.UnityROSPlugins
 {
     public class JointStatesPublisher : MonoBehaviour
     {
-        public string[] jointNames; 
-        public List<GameObject> jointStatePublishObjects;
-        private List<ArticulationBody> jointStatePublishArticulationBodys;
+        public GameObject robotBaseLink;
+        private ArticulationBody robotModelArticulationBody;
+        private ArticulationBody[] robotPartArticulationBody;
         public string topicName = "joint_states";
         private JointStateMsg jointStateMsg = new JointStateMsg();
-        private List<JointStateMsg> jointStateMsgList = new List<JointStateMsg>();
         public GameObject ROSConnectionCommon;
         private Commons commons;
-        private ROSConnection ros;
+        private List<float> positions;
+        private List<float> velocities;
+        private List<float> efforts;
+        private List<int> indexes;
+        private List<string> names;
+        private int startJointIndex;
 
         // Start is called before the first frame update
         void Start()
         {
-            jointStatePublishArticulationBodys = new List<ArticulationBody>();
-            for(int i=0; i<jointStatePublishObjects.Count; ++i){
-                Debug.Log(jointStatePublishObjects[i].name);
-                jointStatePublishArticulationBodys.Add(jointStatePublishObjects[i].GetComponent<ArticulationBody>());
-            }
             commons = ROSConnectionCommon.GetComponent<Commons>();
             commons.ros.RegisterPublisher<JointStateMsg>(topicName, 1);
-            jointStateMsg.name = jointNames;
+
+            robotModelArticulationBody = robotBaseLink.GetComponent<ArticulationBody>();
+            robotPartArticulationBody = robotModelArticulationBody.GetComponentsInChildren<ArticulationBody>();
+
+            indexes = new List<int>();
+            names = new List<string>();
+            for(int i=0; i<robotPartArticulationBody.Length; ++i){
+                if(robotPartArticulationBody[i].jointType != ArticulationJointType.FixedJoint){
+                    indexes.Add(robotPartArticulationBody[i].index);
+                    names.Add(robotPartArticulationBody[i].name);
+                }
+            }
+
+            jointStateMsg.position = new double[indexes.Count];
+            jointStateMsg.velocity = new double[indexes.Count];
+            jointStateMsg.effort = new double[indexes.Count];
+            
+
+            positions = new List<float>();
+            velocities = new List<float>();
+            efforts = new List<float>();
+
+            robotModelArticulationBody.GetJointPositions(positions);
+            robotModelArticulationBody.GetJointPositions(velocities);
+            robotModelArticulationBody.GetJointPositions(efforts);
+            startJointIndex = positions.Count - indexes.Count;
+
+            for(int i=0; i<indexes.Count; ++i){
+                for(int j=i+1; j<indexes.Count; ++j){
+                    if(indexes[i] > indexes[j]){
+                        int tmp = indexes[i];
+                        indexes[i] = indexes[j];
+                        indexes[j] = tmp;
+                        
+                        string s = names[i];
+                        names[i] = names[j];
+                        names[j] = s;
+                    }
+                }
+            }
+
+            jointStateMsg.name = names.ToArray();
         }
 
         // Update is called once per frame
         void FixedUpdate()
         {
             commons.SetTime(jointStateMsg.header.stamp);
-            SetJointStates();
-            // GetQuaternion(jointStatePublishArticulationBodys);
-            // commons.ros.Publish(topicName, jointStateMsg);
+
+            robotModelArticulationBody.GetJointPositions(positions);
+            robotModelArticulationBody.GetJointPositions(velocities);
+            robotModelArticulationBody.GetJointPositions(efforts);
+
+            for(int i=startJointIndex; i<positions.Count; ++i){
+                int index = i-startJointIndex;
+                jointStateMsg.position[index] = positions[i];
+                jointStateMsg.velocity[index] = velocities[i];
+                jointStateMsg.effort[index] = efforts[i];
+            }
+
+            commons.ros.Publish(topicName, jointStateMsg);
+            // for(int i=0; i<publishPositions.Count; ++i){
+            //     Debug.Log($"{jointStateMsg.name[i]}: {publishPositions[i]}");
+            // }
         }
 
-        double[] GetJointPosition(List<ArticulationBody> articulationBodies){
-            List<double> jointPositions = new List<double>();
-            GameObject g;
-            g = articulationBodies[0].GetComponent<GameObject>();
-            foreach(ArticulationBody articulationBody in articulationBodies){
-                List<float> positions = new List<float>();
-                List<int> indies = new List<int>();
-                int i = articulationBody.GetJointPositions(positions);
-                Debug.Log(i);
-                articulationBody.GetDofStartIndices(indies);
-                jointPositions.Add(positions[positions.Count-1]);
-                ArticulationBody[] articulationBody1;
-                articulationBody1 = articulationBody.GetComponentsInChildren<ArticulationBody>();
-                Debug.Log("start");
-                foreach(int index in indies) Debug.Log(index);
-                // foreach(ArticulationBody ab in articulationBody1){
-                //     Debug.Log(ab.name);
-                // }
-                // Debug.Log(indies.Count);
-                // foreach(int index in indies) Debug.Log(index);
-                // Debug.Log(positions.Count);
-                for(int j = 0; j < positions.Count; ++j){
-                    Debug.Log($"{j}: {positions[j]}");
-                }
-                foreach(ArticulationBody ab in articulationBody1){
-                    ab.GetJointPositions(positions);
-                    foreach(double position in positions){
-                        Debug.Log(position);
-                    }
-                }
-                // foreach(float position in positions) Debug.Log(position);
-                Debug.Log("end");
-                // Debug.Log($"{articulationBody.name}: {positions[positions.Count-2]}");
-                // }
-            }
-            return jointPositions.ToArray();
-        }
+        // double[] GetJointPosition(ArticulationBody articulationBody){
+        //     articulationBody.GetJointPositions(positions);
+        //     for(int i=startJointIndex; i<positions.Count; ++i) publishPositions[i-startJointIndex] = positions[i];
+        //     // for(int i=0; i<publishPositions.Count; ++i){
+        //     //     Debug.Log($"{jointStateMsg.name[i]}: {publishPositions[i]}");
+        //     // }
+        //     Debug.Log("end");
+        //     return jointPositions.ToArray();
+        // }
 
         // void GetQuaternion(List<ArticulationBody> articulationBodies){
         //     foreach(ArticulationBody articulationBody in articulationBodies){
@@ -114,7 +140,7 @@ namespace Sample.UnityROSPlugins
         }
 
         void SetJointStates(){
-            jointStateMsg.position = GetJointPosition(jointStatePublishArticulationBodys);
+            // jointStateMsg.position = GetJointPosition(jointStatePublishArticulationBodys);
             // jointStateMsg.velocity = GetJointVelocity(jointStatePublishArticulationBodys);
             // jointStateMsg.effort = GetJointForce(jointStatePublishArticulationBodys);
         }
